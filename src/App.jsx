d174fb5643a77cd23116513fbfeea25f81466f13
Antonella <212781898+antodollparts@users.noreply.github.com>
2026-08-27 14:06:31 -0500

    Proyecto 1: Mi tarjeta de presentación

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Tarjeta from './Tarjeta';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Mi tarjeta de presentación</h1>
+      <Tarjeta />
+    </div>
+  )
+}
+
+export default App
